ca87e775709397b4d3909d635368da2ca0974606
saurav2817 <145346130+saurav2817@users.noreply.github.com>
2025-09-08 14:20:32 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,8 @@ const About = () => {
         <>
             <main className="container mx-auto px-4 py-10">
                 <h1>About Us</h1>
-                <p>Amul packaging provides you with the optimum solution that you are looking for, to promote your brand or product. Along with 30 years of experience, we have collaborated with diversified sectors and brands.</p>
+                
+           
             </main>
         </>
     );
