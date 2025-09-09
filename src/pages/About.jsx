7f7b56533e8980c62be45b8cc5f71679f90b17aa
apurvachalke-hub <apurva.chalke@futuretechpartner.com>
2025-09-09 12:11:16 +0530

    apurva
    about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,95 @@ import React from "react";
 const About = () => {
     return (
         <>
-            <main className="container mx-auto px-4 py-10">
-                <h1>About Us</h1>
-                
+            <main className="">
+                <section className="text-center py-30 bg-[url('/img/bgproduct.jpg')] bg-cover bg-center">
+                    <h1 className="text-3xl font-bold text-white">About Us</h1>
+                </section>
            
+                <section class="py-16 bg-white">
+                    <div class="max-w-7xl mx-auto px-6">
+                        <h2 class="text-3xl md:text-4xl font-bold text-center mb-12">
+                        Our Best Qualities
+                        </h2>
+
+                        <div class="grid md:grid-cols-2 gap-10">
+                       
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-leaf text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Innovative Packaging</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+
+                     
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-tag text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Creative Solutions</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+
+                       
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-tree text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Customer Satisfaction</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+
+                        
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-trophy text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Offers Significant Value</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-trophy text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Continuous Improvement</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+                        <div class="flex items-start space-x-4">
+                            <div class="bg-primary text-white p-4 rounded-md">
+                            <i class="fas fa-trophy text-2xl"></i>
+                            </div>
+                            <div>
+                            <h3 class="text-lg font-semibold mb-2">Conserving Natural Resources</h3>
+                            <p class="text-gray-600">
+                                
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
             </main>
 
             
