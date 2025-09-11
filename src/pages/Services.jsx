27d07593f896f02235d78c48cebaaa64d6980ac5
apurvachalke-hub <apurva.chalke@futuretechpartner.com>
2025-09-11 12:05:14 +0530

    apurva
    services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,8 +3,101 @@ import React from "react";
 const Services = () => {
     return (
         <>
-            <main className="container mx-auto px-4 py-10">
-                <h1>Services</h1>
+            <main className="">
+                <section className="text-center py-30 bg-[url('/img/bgproduct.jpg')] bg-cover bg-center">
+                    <h1 className="text-3xl font-bold text-white">Services</h1>
+                </section>
+
+                <section className="py-16 px-6">
+                    <div className="container mx-auto max-w-7xl ">
+                        <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
+        
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                <img src="/img/serviceICN/food.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Food</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/beverage.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Beverage</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/pet.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Pet</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/pharma.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Pharma</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/agri.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Agri</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/indstrial.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Industrial</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/fitness.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Fitness</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/confectionery.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Confectionery</h4>
+                            </div>
+
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/clothing.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Clothing</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/printingpress.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Printing Press</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/beauty&personalcare.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Beauty & Personal Care</h4>
+                            </div>
+                            
+                            <div class="flex flex-col items-center bg-white shadow-md p-6 rounded-xl">
+                                <div class=" flex items-center justify-center rounded-full ">
+                                    <img src="/img/serviceICN/dairy.png" alt="Food" class="" />
+                                </div>
+                                <h4 class="mt-4 text-xl font-medium text-gray-800">Dairy</h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
